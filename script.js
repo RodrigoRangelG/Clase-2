27bf9ea640d91e5a7edc64bcf642e94fc8a9e2e8
RodrigoRangelG <rocorangelg@gmail.com>
2023-10-25 20:18:00 -0600

    Reto 2, añadir un contador de intentos

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var numeroSecreto = 90;
-
+var contador=0;
 while (numeroSecreto != numeroDigitado) {
+    contador++;
     var numeroDigitado = prompt("Ingresa un número entre 1 y 1000");
 
     if (numeroSecreto == numeroDigitado) 
-        alert("Acertaste");
+        alert("Acertaste\nIntentos: "+contador);
     else if (numeroSecreto > numeroDigitado)
-        alert("Te equivocaste.. el número secreto es mayor que: "+numeroDigitado);
+        alert("Te equivocaste.. el número secreto es mayor que: "+numeroDigitado+"\nIntentos: "+contador);
     else if (numeroSecreto < numeroDigitado)
-        alert("Te equivocaste.. el número secreto es menor que: "+numeroDigitado);
+        alert("Te equivocaste.. el número secreto es menor que: "+numeroDigitado+"\nIntentos: "+contador);
 }

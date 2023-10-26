926c4d6e8ba804bf5efbd99ef735aea3e57c46c4
RodrigoRangelG <rocorangelg@gmail.com>
2023-10-25 20:22:46 -0600

    Reto 3: Agregar una alerta que te diga el máximo numero de intentos que ha tomado una persona en acertar

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 var numeroSecreto = 90;
 var contador=0;
-while (numeroSecreto != numeroDigitado) {
-    contador++;
-    var numeroDigitado = prompt("Ingresa un número entre 1 y 1000");
-
-    if (numeroSecreto == numeroDigitado) 
-        alert("Acertaste\nIntentos: "+contador);
-    else if (numeroSecreto > numeroDigitado)
-        alert("Te equivocaste.. el número secreto es mayor que: "+numeroDigitado+"\nIntentos: "+contador);
-    else if (numeroSecreto < numeroDigitado)
-        alert("Te equivocaste.. el número secreto es menor que: "+numeroDigitado+"\nIntentos: "+contador);
+var maxIntentos=0;
+for(;;){
+    contador = 0;
+    while (numeroSecreto != numeroDigitado) {
+        contador++;
+        var numeroDigitado = prompt("Ingresa un número entre 1 y 1000");
+    
+        if (numeroSecreto == numeroDigitado) 
+            alert("Acertaste\nIntentos: "+contador);
+        else if (numeroSecreto > numeroDigitado)
+            alert("Te equivocaste.. el número secreto es mayor que: "+numeroDigitado+"\nIntentos: "+contador);
+        else if (numeroSecreto < numeroDigitado)
+            alert("Te equivocaste.. el número secreto es menor que: "+numeroDigitado+"\nIntentos: "+contador);
+    }
+    if(maxIntentos<contador)
+        maxIntentos = contador;
+    alert("El número máximo de intentos ha sido: "+maxIntentos);
+    numeroDigitado=null;
 }
+
